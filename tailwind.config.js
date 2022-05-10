c011d45407bc6423ae50fb2327ad09979b3574ed
PastGH <harryricardo666@gmail.com>
2022-05-10 18:13:26 +0800

    update #6 - change pfp on dark mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  darkMode: "class",
+  darkMode: ["class", "media"],
   content: ["index.html", "./pages/thanks.html"],
   theme: {
     container: {
@@ -7,11 +7,20 @@ module.exports = {
       padding: "16px",
     },
     extend: {
+      backgroundImage: (theme) => ({
+        "light-profile": "url(/dist/img/pfp.png)",
+        "dark-profile": "url(/dist/img/pfp_dark.png)",
+      }),
       colors: {
         primary: "#60A5FA",
         secondary: "#64748B",
         dark: "#2563eb",
       },
+    },
+  },
+  variants: {
+    extend: {
+      backgroundImage: ["dark"],
     },
   },
   plugins: [require("@tailwindcss/line-clamp"), require("flowbite/plugin")],
